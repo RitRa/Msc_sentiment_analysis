093a8b46714630327273acda2d12903d7f96c074
ritaraher <ritaraher@gmail.com>
2022-10-28 15:15:16 +0100

    add dashboard

--- notebooks/dash.py
+++ notebooks/dash.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
+#from streamlit_option_menu import option_menu
 #streamlit run dashboard.py
 import pandas as pd
 import numpy as np
@@ -16,15 +16,14 @@ import math
 st.set_page_config(page_title="Tweets", layout="wide")
 
 #columns
-header = st.container()
-col1, col2 = st.columns(2)
-page2 = st.container()
-dataset = st.container()
-features = st.container()
+#header = st.container()
+#col1, col2 = st.columns(2)
+#page2 = st.container()
+#dataset = st.container()
+#features = st.container()
 
 
 #tweets
-
 filename ="https://raw.githubusercontent.com/RitRa/Msc_sentiment_analysis/master/data/df.pkl"
 infile = open(filename,'rb')
 df = pickle.load(infile)

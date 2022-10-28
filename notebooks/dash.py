f57419a72f1833d91b49857c8927648265cb9799
ritaraher <ritaraher@gmail.com>
2022-10-28 15:08:21 +0100

    add dashboard

---
+++ notebooks/dash.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+#streamlit run dashboard.py
+import pandas as pd
+import numpy as np
+import pickle
+
+#data vis
+import plotly.express as px
+
+from dateutil.relativedelta import relativedelta
+import math
+
+
+#setting a wide layout
+st.set_page_config(page_title="Tweets", layout="wide")
+
+#columns
+header = st.container()
+col1, col2 = st.columns(2)
+page2 = st.container()
+dataset = st.container()
+features = st.container()
+
+
+#tweets
+df = pd.read_csv("https://raw.githubusercontent.com/RitRa/Msc_sentiment_analysis/master/data/df.pkl", encoding='latin-1')
+
+df.head()

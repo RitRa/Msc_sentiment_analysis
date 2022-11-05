4683880c9d8123e8ee520fbb3c0b38e30dbf9ff6
ritaraher <ritaraher@gmail.com>
2022-11-05 08:20:07 +0000

    update

--- dashboard/Home.py
+++ dashboard/Home.py
@@ -1,3 +1,4 @@
+
 # install the following packages
 # pip install streamlit
 # pip install plotly
@@ -11,17 +12,18 @@
 
 
 ############################# Libraries start #############################
-# import requests
-# from io import BytesIO
+#import requests
+#from io import BytesIO
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
 import os
 import plotly.express as px
+import joblib
 
-# from dateutil.relativedelta import relativedelta
-# import math
+#from dateutil.relativedelta import relativedelta
+#import math
 
 ############################# Libraries end #############################
 
@@ -32,52 +34,97 @@ st.set_page_config(page_title="Home",
                    layout="wide"
                    )
 
+# columns
+# st.sidebar.markdown("# Models Evaluation")
+#header = st.container()
 
-home_page = st.container()
-col1, col2 = st.columns((1, 4))
+# col1, col2 = st.columns(2)
+# page2 = st.container()
+# dataset = st.container()
+model_evaluation = st.container()
+
 
 ############################# Data start #############################
 # import pickle file
 pickle_xgb = open(
     '/app/msc_sentiment_analysis/dashboard/forecasterxgb.pkl', 'rb')
+
+regressor_xgb = joblib.load(
+    "/app/msc_sentiment_analysis/dashboard/forecasterxgb.sav")
+
 pickle_df = open(
     '/app/msc_sentiment_analysis/dashboard/df_cmp.pkl', 'rb')
 
-full_df = open(
-    '/app/msc_sentiment_analysis/dashboard/df_full.pkl', 'rb')
-
 
 # pickle_xgb = open(
-#     'forecasterxgb.pkl', 'rb')
+#     './forecasterxgb.pkl', 'rb')
 regressor_xgb = pickle.load(pickle_xgb)
 
 
 # pickle_df = open(
-#     'df_cmp.pkl', 'rb')
+#     './df_cmp.pkl', 'rb')
 df_cmp = pickle.load(pickle_df)
-
-
-# full_df = open(
-#     'df_full.pkl', 'rb')
-df_full = pickle.load(full_df)
 ############################# Data end #############################
 
 
 ############################# Model Eval start #############################
-with home_page:
-    st.subheader('Home')
+with model_evaluation:
+    st.title("Model Evaluation")
+    st.write("Select a timeframe to analysis sentiment")
+    steps = 30
+    data_train = df_cmp[:-steps]
+    data_test = df_cmp[-steps:]
 
-    fig_box = px.histogram(df_full, x='compound',
-                           title="Sentiment: Compound from Tweets about UKGOV")
-    fig_box.update_layout(height=400)
-    # disply line Chart
-    st.plotly_chart(fig_box, use_container_width=True)
+    # choice of steps
+    step = {'90 days': 90,
+            '60 days': 60,
+            '30 days': 30,
+            '7 days': 7}
 
-    st.subheader("Top tweeters about 'ukgov'")
-with col1:
+    # dropdown timeframe for model
+    select_timeframe = st.selectbox(
+        'Select timeframe:', ("7 days", "30 days", "60 days", "90 days"))
 
-    top_tweeters = df_full['Username'].value_counts().nlargest(10)
-    top_tweeters
-with col2:
-    fig2 = px.bar(top_tweeters)
-    st.plotly_chart(fig2, use_container_width=True)
+    # st.write('You selected:', select_timeframe)
+
+    #predictions = regressor_xgb.predict(steps=steps)
+
+    # predictions
+
+    def evaluate_model(select_timeframe, step, regressor_xgb):
+        for key, value in step.items():
+
+            if select_timeframe == key:
+                # applies the steps from the user input to the model
+                predictions = regressor_xgb.predict(steps=value)
+
+                st.write("step:", value)
+                st.write("Regressor: XGBRegressor")
+
+                test_data = pd.DataFrame({
+                    'Predicted': predictions,                       # Test Predict Data
+                    'Test': data_test['compound_daily']
+
+                })
+                # plot
+                fig_new = px.line(
+                    test_data, title="Predicted data vs test data using %s" % (value))
+                # streamlit call
+                st.plotly_chart(fig_new, use_container_width=True)
+
+                full_data = pd.DataFrame({
+                    'Predicted': predictions,                       # Test Predict Data
+                    # Test Actual Data
+                    'Test': data_test['compound_daily'],
+                    # Test Actual Data
+                    'Train': data_train['compound_daily']
+                })
+
+                fig_full = px.line(full_data, title="Actual Vs Predicted using %s" % (
+                    value))                        # plot
+                # streamlit call
+                st.plotly_chart(fig_full, use_container_width=True)
+
+    evaluate_model(select_timeframe, step, regressor_xgb)
+
+############################# Model Eval end #############################
